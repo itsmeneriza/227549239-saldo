5f344a03c9521672023a8222a3a863d259e678cb
Neriza Saldo <saldoneriza19@gmail.com>
2024-07-16 21:16:33 +0800

    Initial commit with b1.bat

---
+++ b1.bat
@@ -0,0 +1,23 @@
+@echo off
+@echo open
+rem Neriza A. Saldo
+rem +++++++++++++++++++++++++
+
+rem Open Websites
+start "" "https://www.google.com"
+start "" "https://www.facebook.com"
+start "" "https://www.youtube.com"
+start "" "https://www.canvas.instructure.com"
+start "" "https://www.notion.so"
+
+rem Launch Calculator
+start calc.exe
+
+rem Launch Notepad
+start notepad.exe
+
+rem Initiate Shutdown
+timeout /t 10
+rem shutdown /s /t 30
+
+exit
